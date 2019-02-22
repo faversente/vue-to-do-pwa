7e39fbc7502332a5de2fcb4f861bde695b35b531
faversente <fabio.aversente@accenture.com>
2019-02-22 12:45:08 +0100

    Added basic "Toolbar" view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,54 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      persistent
+      v-model="drawer"
+      app
+    >
+      <v-list>
+        <v-list-tile
+          value="true"
+          v-for="(item, i) in items"
+          :key="i"
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      app
+      :clipped-left="clipped"
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: true,
+      items: [{
+        icon: 'edit',
+        title: 'To do'
+      }],
+      title: 'TwoDigits - ToDo'
+    }
+  },
+  name: 'App'
+}
+</script>
