debb93cf8e54488e9281f653b23f363a6588bd12
faversente <fabio.aversente@accenture.com>
2019-03-05 10:37:05 +0100

    Adapted store to make use of API

--- src/store.js
+++ src/store.js
@@ -1,16 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import api from '@/api/index.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    todos: []
   },
   mutations: {
+    setTodos: (state, todos) => {
+      state.todos = todos
+    },
 
+    addTodo: (state, todo) => {
+      state.todos.push(todo)
+    },
+
+    removeTodo: (state, id) => {
+      state.todos = state.todos.filter(todo => todo.id !== id)
+    }
   },
   actions: {
+    fetchTodos: async ({ commit }) => {
+      let todos = await api.fetchTodos()
+      commit('setTodos', todos)
+    },
 
+    updateTodo: async ({ commit }, todo) => {
+      await api.updateTodo(todo)
+    },
+
+    createTodo: async ({ commit }, todo) => {
+      let id = await api.createTodo(todo)
+      todo.id = id
+      commit('addTodo', todo)
+    },
+
+    deleteTodo: async ({ commit }, id) => {
+      await api.deleteTodo(id)
+      commit('removeTodo', id)
+    }
   }
 })
